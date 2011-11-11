fa45f0fa3c7a3ff2aaa1903b64410cb0bc3805b8
Drew Skau <dwskau@gmail.com>
2011-11-11 13:08:02 -0500

    goodbye

--- java/my_magnum_opus.java
+++ java/my_magnum_opus.java
@@ -22,7 +22,7 @@
 
 public class Main {
         public static void main(String[] args){
-                System.out.println("Hello, World!\n"); //Hello world
+                System.out.println("Goodbye, World!\n"); //Hello world
         }
 }
 
